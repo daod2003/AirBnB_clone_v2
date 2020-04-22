04d73b283f6b3d3002bdd461b884b72e4df5b271
daod2003 <1253@holbertonschool.com>
2020-04-22 16:05:51 +0000

    created a new route to python that not get any parameter

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -27,5 +27,15 @@ def cIsFun(text):
     """
     return 'C ' + text.replace('_', ' ')
 
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythonIsCool(text='is cool'):
+    """
+    display “Python ” followed by the value of the text
+    variable (replace underscore _ symbols with a space )
+    """
+    return 'Python ' + text.replace('_', ' ')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
